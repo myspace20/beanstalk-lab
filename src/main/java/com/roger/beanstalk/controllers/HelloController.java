32fd27671ab7cab42485582f45bb72af33721272
rogersatsi <rogersatsi98@gmail.com>
2025-09-04 14:06:23 +0000

    feat: reuse actions in hello response

--- src/main/java/com/roger/beanstalk/controllers/HelloController.java
+++ src/main/java/com/roger/beanstalk/controllers/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String index() {
-        return "Hello World! from Roger's Beanstalk!";
+        return "Hello World! from Roger's Beanstalk! Github Actions";
     }
 }
