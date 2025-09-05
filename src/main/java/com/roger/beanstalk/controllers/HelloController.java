b6e8f47d90ebfbecc75ed1f23b90a6405c5e948f
rogersatsi <rogersatsi98@gmail.com>
2025-09-05 09:03:08 +0000

    feat: update return values to v2

--- src/main/java/com/roger/beanstalk/controllers/HelloController.java
+++ src/main/java/com/roger/beanstalk/controllers/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String index() {
-        return "Hello World! from Roger's Beanstalk! Github Actions";
+        return "Hello World! from Roger's Beanstalk! Github Actions version 2";
     }
 }
